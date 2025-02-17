39b4a6d0fe1fb6844ea16b02915e848eca944357
mackliu <macklun@gmail.com>
2025-02-17 14:36:16 +0800

    修正刪除功能

--- front/buycart.php
+++ front/buycart.php
@@ -54,7 +54,7 @@ if(!isset($_SESSION['Mem'])){
 <script>
     function delCart(id){
         $.post("./api/delcart.php",{id},function(){
-            location.reload();
+            location.href='?do=buycart';
         })
     }
 </script>
